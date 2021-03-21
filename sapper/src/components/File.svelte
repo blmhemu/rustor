fb1a8cf231ed0e7101383fffc932bee855f81e0a
Hemanth Bollamreddi <blmhemu@gmail.com>
2021-03-21 13:08:27 +0530

    Added sapper sveltekit has issues with back button

---
+++ sapper/src/components/File.svelte
@@ -0,0 +1,53 @@
+<div
+  class="group cursor-pointer flex flex-col rounded-lg p-2 space-y-2 border-4 border-transparent hover:border-blue-400"
+  on:dblclick
+>
+  {#if fileData.is_dir}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-folder"
+      ><path
+        d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"
+      /></svg
+    >
+  {:else}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="100"
+      height="100"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-file"
+      ><path
+        d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"
+      /><polyline points="13 2 13 9 20 9" /></svg
+    >
+  {/if}
+  <p class="text-lg text-center truncate text-gray-600">
+    {fileData.name}
+  </p>
+  <button
+    class="rounded-lg bg-red-300 transition-opacity delay-100 duration-100 opacity-0 ease-in-out group-hover:opacity-100 ring ring-pink-600"
+    >Download</button
+  >
+</div>
+
+<script lang="ts" context="module">
+  export type FileData = { name: string; is_dir: boolean; path: boolean };
+</script>
+
+<script lang="ts">
+  export let fileData: FileData;
+</script>
---
+++ sapper/src/routes/files/index.svelte
@@ -0,0 +1,53 @@
+<div
+  class="grid grid-flow-row-dense gap-2 grid-cols-3 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7"
+>
+  {#each files as file}
+    <File fileData={file} on:dblclick={() => gotoOrGet(file)} />
+  {/each}
+</div>
+
+<!-- TODO: See if this can be merged into [...slug].svelte -->
+<script lang="ts" context="module">
+  export async function preload(page, session) {
+    const res = await this.fetch(`http://127.0.0.1:3030/api/ls`);
+    if (res.ok) {
+      const article = await res.json();
+      const files = article as FileData[];
+      return {
+        files,
+      };
+    }
+    return {
+      status: res.status,
+      error: new Error(`Could not find the resource.`),
+    };
+  }
+</script>
+
+<script lang="ts">
+  import File from '../../components/File.svelte';
+  import type { FileData } from '../../components/File.svelte';
+  import { goto } from '@sapper/app';
+
+  export let files: FileData[];
+
+  async function gotoOrGet(fileData: FileData) {
+    if (fileData.is_dir) {
+      await goto('/files/' + fileData.path);
+    } else {
+      fetch('http://127.0.0.1:3030/api/dl?path=' + fileData.path)
+        .then((resp) => resp.blob())
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          // the filename you want
+          a.download = fileData.name;
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+        });
+    }
+  }
+</script>
